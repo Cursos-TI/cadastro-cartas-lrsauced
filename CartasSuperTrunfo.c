3e22b045bdb012dc8bb8566282bde125ed0a8732
lrsauced <lrsauced@gmail.com>
2026-02-18 20:32:42 +0000

    Criei o printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,6 +5,7 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
+    printf("Olá mundo teste 2\n\n\n\n\n");
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   // Área para entrada de dados
